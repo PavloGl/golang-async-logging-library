ee8b6a4dab8d51170f41f8617e1bdf53c1501a40
PavloGl <pawel.glushko@gmail.com>
2022-02-05 22:53:40 +0100

    * Fixed: write method error handling

--- alog.go
+++ alog.go
@@ -48,8 +48,12 @@ func (al Alog) formatMessage(msg string) string {
 	return fmt.Sprintf("[%v] - %v", time.Now().Format("2006-01-02 15:04:05"), msg)
 }
 
-func (al Alog) write(msg string, wg *sync.WaitGroup) {
-	al.dest.Write([]byte(al.formatMessage(msg)))
+func (al Alog) write(msg string, wg *sync.WaitGroup) error {
+	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
+	if err != nil {
+		al.errorCh <- err
+	}
+	return nil
 }
 
 func (al Alog) shutdown() {
