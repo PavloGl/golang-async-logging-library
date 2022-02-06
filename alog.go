b873aa5fe1946a1f74de12846720330dffc2d92d
PavloGl <pawel.glushko@gmail.com>
2022-02-06 12:30:35 +0100

    Added waiting for all message to be written before shutting down

--- alog.go
+++ alog.go
@@ -41,16 +41,18 @@ func New(w io.Writer) *Alog {
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
+	wg := &sync.WaitGroup{}
 messageLoop:
 	for {
 		select {
-		case <-al.msgCh:
-			go al.write(<-al.msgCh, nil)
+		case msg := <-al.msgCh:
+			wg.Add(1)
+			go al.write(msg, wg)
 		case <-al.shutdownCh:
+			wg.Wait()
 			al.shutdown()
 			break messageLoop
 		}
-
 	}
 }
 
@@ -62,13 +64,14 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) error {
+	defer wg.Done()
 	al.m.Lock()
 	defer al.m.Unlock()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
 	if err != nil {
-		go func() {
+		go func(err error) {
 			al.errorCh <- err
-		}()
+		}(err)
 	}
 
 	return nil
